b6601f47109ae0536369d1dc2f3c1441d05d0ad4
zonewave <ybzhang17@163.com>
2024-09-21 11:17:51 +0800

    feat(listnode): add get index method

--- py/src/precompiled/listnode.py
+++ py/src/precompiled/listnode.py
@@ -1,27 +1,51 @@
 from dataclasses import dataclass
 from functools import reduce
-from typing import Optional
+from typing import Optional, Tuple,Union
 
 
 @dataclass
 class ListNode(object):
-  val: int
-  next: Optional['ListNode']
+    val: int
+    next: Optional['ListNode']
 
-  def __eq__(self, other: 'ListNode') -> bool:
-    if not isinstance(other, ListNode):
-      return False
-    return self.val == other.val and (self.next == other.next)
+    def __eq__(self, other: 'ListNode') -> bool:
+        if not isinstance(other, ListNode):
+            return False
+        return self.val == other.val and (self.next == other.next)
 
-  @staticmethod
-  def array_to_list_node(arr:list[int])->Optional['ListNode']:
-   return reduce(lambda acc, val: ListNode(val,acc), reversed(arr), None)
+    def index(self, idx: int)->Optional['ListNode']:
+        """
+        返回链表第 idx 个节点， 下标从 0 开始。
+        Returns the idx node of the linked list, starts at 0.
+        """
+        if idx <0:
+            return None
+        n=self
+        for _ in range(1,idx+1):
+            if n is None:
+                return None
+            n =n.next
+        return n
 
-  @staticmethod
-  def arrays_to_linked_list(*arr_list:list[int])->tuple['ListNode']:
-    return tuple(ListNode.array_to_list_node(arr) for arr in arr_list)
+    @staticmethod
+    def array_to_list_node(arr: list[int]) -> Optional['ListNode']:
+        """
+        将数组转换为链表节点。
+        Converts an array to a linked list node.
+        """
+        return reduce(lambda acc, val: ListNode(val, acc), reversed(arr), None)
 
+    @staticmethod
+    def arrays_to_linked_list(*arr_list: list[int]) -> Tuple[Union['ListNode', None], ...]:
+        """
+        将多个数组转换为链表节点元组。
+        Converts multiple arrays to a tuple of linked list nodes.
 
+        参数:
+        *arr_list (list[int]): 输入的多个整数数组 (Input multiple integer arrays).
 
-
-
+        返回:
+        Tuple[Optional[ListNode]]: 链表头节点的元组，每个数组对应一个链表头节点。
+        A tuple of head nodes of the linked lists, with each array corresponding to a linked list head node.
+        """
+        return tuple(ListNode.array_to_list_node(arr) for arr in arr_list)
--- py/tests/test_listnode.py
+++ py/tests/test_listnode.py
@@ -5,37 +5,46 @@ from src.precompiled.listnode import ListNode
 
 class TestListNode(TestCase):
     def test_eq_same_values(self):
-       node1 = ListNode(1, None)
-       node2 = ListNode(1, None)
-       self.assertEqual(node1, node2)
+        node1 = ListNode(1, None)
+        node2 = ListNode(1, None)
+        self.assertEqual(node1, node2)
 
     def test_eq_different_values(self):
-       node1 = ListNode(1, None)
-       node2 = ListNode(2, None)
-       self.assertNotEqual(node1, node2)
+        node1 = ListNode(1, None)
+        node2 = ListNode(2, None)
+        self.assertNotEqual(node1, node2)
 
     def test_eq_different_structure(self):
-       node1 = ListNode(1, ListNode(2, None))
-       node2 = ListNode(1, ListNode(3, None))
-       self.assertNotEqual(node1, node2)
+        node1 = ListNode.array_to_list_node([1,2])
+        node2 = ListNode.array_to_list_node([1,3])
+        self.assertNotEqual(node1, node2)
 
     def test_array_to_list_node(self):
-       arr = [1, 2, 3]
-       head = ListNode.array_to_list_node(arr)
-       self.assertEqual(head.val, 1)
-       self.assertEqual(head.next.val, 2)
-       self.assertEqual(head.next.next.val, 3)
-       self.assertIsNone(head.next.next.next)
+        arr = [1, 2, 3]
+        head = ListNode.array_to_list_node(arr)
+        self.assertEqual(head.val, 1)
+        self.assertEqual(head.next.val, 2)
+        self.assertEqual(head.next.next.val, 3)
+        self.assertIsNone(head.next.next.next)
 
     def test_arrays_to_linked_list(self):
-       arr1 = [1, 2, 3]
-       arr2 = [4, 5]
-       linked_lists = ListNode.arrays_to_linked_list(arr1, arr2)
+        arr1 = [1, 2, 3]
+        arr2 = [4, 5]
+        (l1,l2) = ListNode.arrays_to_linked_list(arr1, arr2)
+        self.assertEqual(l1.val, 1)
+        self.assertEqual(l2.val, 4)
+        self.assertIsNone(l1.next.next.next)  # arr1 should end here
+        self.assertEqual(l2.next.val, 5)
+        self.assertIsNone(l2.next.next)  # arr2 should end here
 
-       self.assertEqual(linked_lists[0].val, 1)
-       self.assertEqual(linked_lists[1].val, 4)
-       self.assertIsNone(
-          linked_lists[0].next.next.next)  # arr1 should end here
-       self.assertEqual(linked_lists[1].next.val, 5)
-       self.assertIsNone(linked_lists[1].next.next)  # arr2 should end here
 
+    def test_index_valid(self):
+        head =ListNode.array_to_list_node([1,2,3])
+        self.assertEqual(head.index(0).val, 1)
+        self.assertEqual(head.index(1).val, 2)  #
+        self.assertEqual(head.index(2).val, 3)  #
+
+        self.assertIsNone(head.index(3))
+        self.assertIsNone(head.index(10))
+
+        self.assertIsNone(head.index(-1))
