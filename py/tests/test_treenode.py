818b03754ce9e9c0671bdfc5efb28951da5f88a8
zonewave <ybzhang17@163.com>
2024-09-23 13:32:29 +0800

    feat(tree): add new tree method

---
+++ py/tests/test_treenode.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+from src.precompiled.treenode import TreeNode
+
+
+class TestTreeNode(TestCase):
+    def test_arr_to_tree(self):
+        root = TreeNode.array_to_tree([2, 3, 4])
+        self.assertEqual(root.left.val, 3)
+        self.assertEqual(root.right.val, 4)
+        self.assertEqual(root.val, 2)
