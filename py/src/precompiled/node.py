99c9c26a535c4faf33b970a0912efdef91ad65b0
zonewave <ybzhang17@163.com>
2024-09-23 15:01:18 +0800

    feat(node): add with random node

---
+++ py/src/precompiled/node.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+from typing import Optional, List, Dict
+
+from src.precompiled.listnode import ListNode
+from src.precompiled.treenode import TreeNode
+
+
+@dataclass
+class Node(TreeNode, ListNode):
+    random: Optional['Node']
+
+    def __init__(self, val):
+        TreeNode.__init__(self, val)
+        ListNode.__init__(self, val, None)
+        self.random = None
+
+    @staticmethod
+    def array_to_list_node(arr: List[int]) -> Optional['Node']:
+        return ListNode.array_to_list_node(arr)
+
+    @staticmethod
+    def arr_to_tree(vals: List[int]) -> Optional['Node']:
+        return TreeNode.array_to_tree(vals)
+
+    @staticmethod
+    def arr_to_random_list(arr: List[List[int]]) -> Optional['Node']:
+        if not arr:
+            return None
+        index_to_map: Dict[int, 'Node'] = {}
+        current = head = Node(0)
+        for idx, n in enumerate(arr):
+            current.next = Node(n[0])
+            current = current.next
+            index_to_map[idx] = current
+
+        current = head.next
+        for n in arr:
+            if n[1] is not None:
+                current.random = index_to_map[n[1]]
+            current = current.next
+
+        return head.next
