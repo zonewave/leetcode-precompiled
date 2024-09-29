25c67bee112eeabe6f3e049b355215c8af206171
zonewave <ybzhang17@163.com>
2024-09-29 15:29:15 +0800

    feat: add cpp snippets

--- py/src/precompiled/tools/print_vscode_snippets.py
+++ py/src/precompiled/tools/print_vscode_snippets.py
@@ -1,7 +1,6 @@
 import json
 
-if __name__ == '__main__':
-    code = """
+py = """
 from precompiled.all import *
 @dataclass
 class Args:
@@ -32,6 +31,43 @@ class TestSolution(TestCase):
                 # may be replace func name
                 self.assertEqual(s.$6(**case.args.__dict__), case.want)
 """
-    code_lines = code.split("\n")
+
+cpp = """
+struct Args {
+    mutable ? ;
+    mutable ? ;
+};
+
+struct LTestCase {
+    string name;
+    Args args;
+    ? want;
+};
+
+TEST_CASE("test cases") {
+    Solution s;
+
+    vector<LTestCase> cases = {
+        // todo
+        {"1", {}, },
+    };
+
+    for (const auto& case_ : cases) {
+        SUBCASE(case_.name.c_str()) {
+            vector<int> result = s.?(case_.args.arg1, case_.args.arg2);
+            CHECK(result == case_.want); 
+        }
+    }
+}
+"""
+
+
+def print_json(tmpl):
+    code_lines = tmpl.split("\n")
     code_json = json.dumps(code_lines)
     print(code_json)
+
+
+if __name__ == '__main__':
+    # print(py)
+    print_json(cpp)
